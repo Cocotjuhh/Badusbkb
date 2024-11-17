d51c889b2192cd93b33597185194836965c65cd7
Cocotjuhh <155767892+Cocotjuhh@users.noreply.github.com>
2024-11-17 20:44:31 +0100

    Update Install-File.ps1

--- Win/Install/Install-File.ps1
+++ Win/Install/Install-File.ps1
@@ -1,3 +1,3 @@
-Invoke-WebRequest -Uri "https://example.com/path/to/your/file.exe" -OutFile "$env:USERPROFILE\Downloads\File.exe"; if (-not (Test-Path "$env:USERPROFILE\Downloads\YourApp")) { New-Item -ItemType Directory -Path "$env:USERPROFILE\Downloads\YourApp" }; Copy-Item -Path "$env:USERPROFILE\Downloads\File.exe" -Destination "$env:USERPROFILE\Downloads\YourApp\File.exe" -Force; Start-Process "$env:USERPROFILE\Downloads\YourApp\File.exe"
+Invoke-WebRequest -Uri "https://ubuntu.com/download/desktop/thank-you?version=24.10&architecture=amd64" -OutFile "$env:USERPROFILE\Downloads\File.exe"; if (-not (Test-Path "$env:USERPROFILE\Downloads\YourApp")) { New-Item -ItemType Directory -Path "$env:USERPROFILE\Downloads\YourApp" }; Copy-Item -Path "$env:USERPROFILE\Downloads\File.exe" -Destination "$env:USERPROFILE\Downloads\YourApp\File.exe" -Force; Start-Process "$env:USERPROFILE\Downloads\YourApp\File.exe"
 
 
