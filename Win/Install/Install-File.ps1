731546fa989539d4b9421cbfff8799f7627603df
Cocotjuhh <155767892+Cocotjuhh@users.noreply.github.com>
2024-11-17 20:54:14 +0100

    Update Install-File.ps1

--- Win/Install/Install-File.ps1
+++ Win/Install/Install-File.ps1
@@ -1,3 +1,3 @@
-Invoke-WebRequest -Uri "https://releases.ubuntu.com/24.10/ubuntu-24.10-desktop-amd64.iso?_ga=2.51572907.34575919.1731872626-541167693.1731872626" -OutFile "$env:USERPROFILE\Downloads\File.exe"; if (-not (Test-Path "$env:USERPROFILE\Downloads\YourApp")) { New-Item -ItemType Directory -Path "$env:USERPROFILE\Downloads\YourApp" }; Copy-Item -Path "$env:USERPROFILE\Downloads\File.exe" -Destination "$env:USERPROFILE\Downloads\YourApp\File.exe" -Force; Start-Process "$env:USERPROFILE\Downloads\YourApp\File.exe" -Wait
+Invoke-WebRequest -Uri "https://builds.parsec.app/package/parsec-windows.exe" -OutFile "$env:USERPROFILE\Downloads\File.exe"; if (-not (Test-Path "$env:USERPROFILE\Downloads\YourApp")) { New-Item -ItemType Directory -Path "$env:USERPROFILE\Downloads\YourApp" }; Copy-Item -Path "$env:USERPROFILE\Downloads\File.exe" -Destination "$env:USERPROFILE\Downloads\YourApp\File.exe" -Force; Start-Process "$env:USERPROFILE\Downloads\YourApp\File.exe" -Wait
 
 
