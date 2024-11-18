4355247a6e385bf1c273dd780b978e549810d961
Cocotjuhh <155767892+Cocotjuhh@users.noreply.github.com>
2024-11-18 08:38:56 +0100

    Update Ultravnc1.ps1

--- Win/ultravnc/Ultravnc1.ps1
+++ Win/ultravnc/Ultravnc1.ps1
@@ -1,1 +1,1 @@
-https://uvnc.eu/download/1436/UltraVNC_1436_X86_Setup.exe" -OutFile "$env:temp\UltraVNC_1436_X86_Setup.exe
+Invoke-WebRequest -Uri "https://uvnc.eu/download/1436/UltraVNC_1436_X86_Setup.exe" -OutFile "$env:temp\UltraVNC_1436_X86_Setup.exe"
