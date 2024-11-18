fe01b4bb23bea231038ab9d6c005962213e3cd87
Cocotjuhh <155767892+Cocotjuhh@users.noreply.github.com>
2024-11-18 16:32:27 +0100

    Update Ultravnc3.ps1

--- Win/ultravnc/Ultravnc3.ps1
+++ Win/ultravnc/Ultravnc3.ps1
@@ -1,1 +1,1 @@
-Start-Process "$env:USERPROFILEC:\Program Files\uvnc bvba\UltraVNC" -ArgumentList "-connect 192.168.178.214:5500 -password Zhuan2010!"
+Start-Process "$env:USERPROFILEC:C:\Program Files\uvnc bvba\UltraVNC\vncviewer.exe" -ArgumentList "-connect 192.168.178.214:5500 -password Zhuan2010!"
